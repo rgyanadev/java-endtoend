811ec503d1df7c9ce9d8f00ebcb2130ad4ecb5f0
Gyana Ranjan Rout <rgyana.dev@gmail.com>
2026-04-23 07:57:18 +0530

    new statements

--- FirstClass.java
+++ FirstClass.java
@@ -1,5 +1,9 @@
 public class FirstClass {
     public static void main(String[] args){
         System.out.println("Gyana Ranjan Rout");
+        System.out.println("Gyana Ranjan Rout1");
+        System.out.println("Gyana Ranjan Rout2");
+        System.out.println("Gyana Ranjan Rout3");
+        System.out.println("Gyana Ranjan Rout4");
     }
 }
